e7e3c694c2797ddc73c4481a0d5a5df90323eb11
Andrey <avlopatin@users.noreply.github.com>
2019-03-24 18:27:08 +0600

    HT1: Track auth state changed

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,8 +1,6 @@
-import firebase from 'firebase/app'
-
 export const appName = 'adv-react-21-03'
 
-const config = {
+export const config = {
   apiKey: 'AIzaSyB9ny-yu42btJ4GVe-8EB-Xq4IOeOH-XlY',
   authDomain: 'adv-react-7e681.firebaseapp.com',
   databaseURL: 'https://adv-react-7e681.firebaseio.com',
@@ -10,5 +8,3 @@ const config = {
   storageBucket: 'adv-react-7e681.appspot.com',
   messagingSenderId: '833111569423'
 }
-
-firebase.initializeApp(config)
