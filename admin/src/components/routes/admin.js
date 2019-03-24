0b3c6bfbf858a00b66f256221ae203e0a4c6abc2
Kirill <umixupo@gmail.com>
2019-03-24 18:46:03 +0300

    hm done except for task 1

--- admin/src/components/routes/admin.js
+++ admin/src/components/routes/admin.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { isAuthorized } from '../../ducks/auth'
+import { addPerson } from '../../ducks/addPerson'
+import AddPerson from '../adminForms/AddPerson'
 
 class AdminPage extends Component {
   static propTypes = {}
@@ -10,11 +12,19 @@ class AdminPage extends Component {
     return (
       <div>
         <h1>Admin</h1>
+        <AddPerson onSubmit={this.handleAddPerson} />
       </div>
     )
   }
+  handleAddPerson = ({ name, surname, email }) =>
+    this.props.addPerson(name, surname, email)
 }
 
-export default connect((state) => ({
-  isAuthorized: isAuthorized(state)
-}))(AdminPage)
+export default connect(
+  (state) => ({
+    isAuthorized: isAuthorized(state)
+  }),
+  {
+    addPerson
+  }
+)(AdminPage)
--- admin/src/redux/reducer.js
+++ admin/src/redux/reducer.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import { reducer as form } from 'redux-form'
 import authReducer, { moduleName as authModule } from '../ducks/auth'
+import addPersonReducer, {
+  moduleName as addPersonModule
+} from '../ducks/addPerson'
 import history from '../history'
 
 export default combineReducers({
   router: connectRouter(history),
   form,
-  [authModule]: authReducer
+  [authModule]: authReducer,
+  [addPersonModule]: addPersonReducer
 })
