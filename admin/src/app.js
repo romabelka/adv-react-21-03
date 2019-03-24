ada79ccaea553478a19f9ae4bf0466f0dfeaf2e6
Eugeny Ternovoy <ternovoy-ev@yandex.ru>
2019-03-24 17:59:56 +0300

    Make reusable logic for protecting pages

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import { Route, NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { isAuthorized } from './ducks/auth'
 import AuthPage from './components/routes/auth'
 import AdminPage from './components/routes/admin'
+import notAuthPage from './components/common/notAuth'
 
 class App extends Component {
   static propTypes = {}
 
   render() {
+    const { isAuthorized } = this.props
+    const protectedPage = (component) =>
+      isAuthorized ? component : notAuthPage
+
     return (
       <div>
         <nav>
@@ -25,11 +32,13 @@ class App extends Component {
         </nav>
         <section>
           <Route path="/auth" component={AuthPage} />
-          <Route path="/admin" component={AdminPage} />
+          <Route path="/admin" component={protectedPage(AdminPage)} />
         </section>
       </div>
     )
   }
 }
 
-export default App
+export default connect((state) => ({
+  isAuthorized: isAuthorized(state)
+}))(App)
