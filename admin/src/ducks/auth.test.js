d26f9a1513bdedec2074774a7db94b39ca879fe5
kosty2979 <kosty2979@gmail.com>
2019-03-27 18:33:33 +0200

    added test for signInSaga

--- admin/src/ducks/auth.test.js
+++ admin/src/ducks/auth.test.js
@@ -1,12 +1,17 @@
-import { put, call } from 'redux-saga/effects'
+import { put, call, take } from 'redux-saga/effects'
 import api from '../services/api'
 import {
   signUpSaga,
   signInSaga,
   signUp,
+  signIn,
   SIGN_UP_START,
   SIGN_UP_SUCCESS,
-  SIGN_UP_ERROR
+  SIGN_UP_ERROR,
+  SIGN_IN_START,
+  SIGN_IN_SUCCESS,
+  SIGN_IN_ERROR,
+  SIGN_IN_REQUEST
 } from './auth'
 
 const data = {
@@ -23,6 +28,7 @@ const person = {
 const error = new Error('error')
 
 const signUpAction = signUp(data.email, data.password)
+const signInAction = signIn(data.email, data.password)
 
 describe('Auth Duck', () => {
   describe('signUpSaga', () => {
@@ -47,6 +53,34 @@ describe('Auth Duck', () => {
         put({ type: SIGN_UP_ERROR, error: error })
       )
       expect(gen.next().done).toBe(true)
+    })
+  })
+  describe('signInSaga', () => {
+    it('should sing in success', () => {
+      const gen = signInSaga(signInAction)
+      const next = gen.next().value
+      expect(next).toEqual(take(SIGN_IN_REQUEST))
+      expect(gen.next(next).value).toEqual(put({ type: SIGN_IN_START }))
+      expect(gen.next().value).toEqual(
+        call(api.signIn, next.email, next.password)
+      )
+      expect(gen.next(person).value).toEqual(
+        put({ type: SIGN_IN_SUCCESS, payload: { user: person } })
+      )
+      expect(gen.next().done).toBe(false)
+    })
+    it('should sing in error', () => {
+      const gen = signInSaga(signInAction)
+      const next = gen.next().value
+      expect(next).toEqual(take(SIGN_IN_REQUEST))
+      expect(gen.next(next).value).toEqual(put({ type: SIGN_IN_START }))
+      expect(gen.next().value).toEqual(
+        call(api.signIn, next.email, next.password)
+      )
+      expect(gen.throw(error).value).toEqual(
+        put({ type: SIGN_IN_ERROR, error: error })
+      )
+      expect(gen.next().done).toBe(false)
     })
   })
 })
