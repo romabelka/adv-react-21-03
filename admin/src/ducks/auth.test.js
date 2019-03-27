07f2c0e52ee661d119094554324f0e229abbfc83
Vladislav Popov <vllad1108@gmail.com>
2019-03-27 19:12:16 +0600

    Added sign in test

--- admin/src/ducks/auth.test.js
+++ admin/src/ducks/auth.test.js
@@ -2,18 +2,20 @@ import { put, call } from 'redux-saga/effects'
 import {
   signUp,
   signUpSaga,
+  signIn,
+  signInSaga,
   SIGN_UP_START,
   SIGN_UP_SUCCESS,
-  SIGN_UP_ERROR
+  SIGN_IN_START,
+  SIGN_IN_SUCCESS
 } from './auth'
 import api from '../services/api'
-import { generateId } from './utils'
 
 describe('Auth Duck', () => {
   describe('Sign up', () => {
     it('should sign up user', () => {
       const userData = {
-        email: `test${generateId()}@example.com`,
+        email: 'test@example.com',
         password: '123456789'
       }
 
@@ -34,6 +36,58 @@ describe('Auth Duck', () => {
       expect(gen.next(user).value).toEqual(
         put({
           type: SIGN_UP_SUCCESS,
+          payload: { user }
+        })
+      )
+    })
+  })
+  describe('Sign in', () => {
+    it('signInSaga should sign in user', () => {
+      const userData = {
+        email: 'test1@example.com',
+        password: '123456789'
+      }
+
+      const gen = signInSaga(userData)
+
+      expect(gen.next().value).toEqual(
+        put({
+          type: SIGN_IN_START
+        })
+      )
+
+      const user = call(api.signIn, userData.email, userData.password)
+
+      expect(gen.next().value).toEqual(user)
+
+      expect(gen.next(user).value).toEqual(
+        put({
+          type: SIGN_IN_SUCCESS,
+          payload: { user }
+        })
+      )
+    })
+    it('signInSagaLoop should sign in user', () => {
+      const userData = {
+        email: 'test1@example.com',
+        password: '123456789'
+      }
+
+      const gen = signInSaga(userData)
+
+      expect(gen.next().value).toEqual(
+        put({
+          type: SIGN_IN_START
+        })
+      )
+
+      const user = call(api.signIn, userData.email, userData.password)
+
+      expect(gen.next().value).toEqual(user)
+
+      expect(gen.next(user).value).toEqual(
+        put({
+          type: SIGN_IN_SUCCESS,
           payload: { user }
         })
       )
