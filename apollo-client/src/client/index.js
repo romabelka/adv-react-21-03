96a95448fbf4519642bc5d81a8282f5a5b798099
Vladislav Popov <vllad1108@gmail.com>
2019-04-07 21:15:50 +0600

    Added creating event

--- apollo-client/src/client/index.js
+++ apollo-client/src/client/index.js
@@ -1,7 +1,11 @@
 import ApolloClient from 'apollo-boost'
 import gql from 'graphql-tag'
-
-const client = new ApolloClient({})
+const client = new ApolloClient({
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log('graphQLErrors', graphQLErrors)
+    console.log('networkError', networkError)
+  }
+})
 
 //dev only
 window.client = client
