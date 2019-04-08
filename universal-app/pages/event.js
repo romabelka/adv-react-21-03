aaf48dceaccbb83bdaf3c9616e124d2a414cf803
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-04-08 20:47:38 +0300

    add custom routing

--- universal-app/pages/event.js
+++ universal-app/pages/event.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Link from 'next/link'
 //import eventQuery from '../queries/event'
 
 class Event extends Component {
@@ -13,6 +14,9 @@ class Event extends Component {
     render() {
         return (
             <div>
+                <Link href="/event-list">
+                    <a>Event List</a>
+                </Link>
                 <h1>{this.props.id}</h1>
             </div>
         )
