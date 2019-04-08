bc92e002b577e512b0b142a198e32517c9477754
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-04-08 20:39:51 +0300

    add csutom express server

--- universal-app/pages/event.js
+++ universal-app/pages/event.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react'
+//import eventQuery from '../queries/event'
 
 class Event extends Component {
     static propTypes = {
 
     }
 
-    static async getInitialProps({}) {
-        const res = await fetch('http://localhost:5000/graphql' , {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ query: '{ allEvents { title id } }' })
-        })
-
-        const { data } = await res.json()
-
-        return {
-            events: data.allEvents
-        }
+    static getInitialProps({ query: { id } }) {
+        return { id }
     }
 
     render() {
         return (
-            <ul>
-                {this.props.events.map(event => <li key={event.id}>{event.title}</li>)}
-            </ul>
+            <div>
+                <h1>{this.props.id}</h1>
+            </div>
         )
     }
 }
---
+++ universal-app/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const nextApp = next({ dev })
+const handle = nextApp.getRequestHandler()
+
+nextApp.prepare().then(() => {
+    const server = express()
+
+    server.get('/event/:id', (req, res) => {
+        return nextApp.render(req, res, '/event', { id: req.params.id })
+    })
+
+    server.get('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    server.listen(port, err => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${port}`)
+    })
+})
