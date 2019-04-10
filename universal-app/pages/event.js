1dc38b434ec8d54f5b501106d3191c8b32c4400e
Vladislav Popov <vllad1108@gmail.com>
2019-04-10 23:57:38 +0600

    Added showing event information. Added links to people from events

--- universal-app/pages/event.js
+++ universal-app/pages/event.js
@@ -7,17 +7,43 @@ class Event extends Component {
 
     }
 
-    static getInitialProps({ query: { id } }) {
-        return { id }
+    static async getInitialProps({ query: { id } }) {
+      const res = await fetch('http://localhost:5000/graphql' , {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ query: `{ event(id: "${id}") { title where url people { firstName id } } }` })
+        })
+
+      const { data } = await res.json()
+
+      return {
+        id,
+        ...data.event
+      }
     }
 
     render() {
+        const { id, title, where, url, when, people } = this.props;
+
         return (
             <div>
                 <Link href="/event-list">
                     <a>Event List</a>
                 </Link>
-                <h1>{this.props.id}</h1>
+                <h1>{title}</h1>
+                <div>{where}</div>
+                <div>{url}</div>
+                <ul>
+                  {people && people.map(person => (
+                    <li key={person.id}>
+                      <Link href={`/people?id=${person.id}`} as={`/person/${person.id}`}>
+                        <a>
+                          {person.firstName}
+                        </a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
             </div>
         )
     }
