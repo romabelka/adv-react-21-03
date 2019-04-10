520968eb49e2c0d93cba3a84a0a5ae1710106a40
koty2979 <kosty2979@gmail.com>
2019-04-10 11:58:29 +0300

    added load event data  for eventPage

--- universal-app/pages/event.js
+++ universal-app/pages/event.js
@@ -7,17 +7,35 @@ class Event extends Component {
 
     }
 
-    static getInitialProps({ query: { id } }) {
-        return { id }
+    static async getInitialProps({ query: { id } }) {
+        const res = await fetch('http://localhost:5000/graphql' , {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query: `{ event(id: "${id}") { title id where url } }` })
+        })
+
+        const { data } = await res.json()
+
+        return {
+            event: data.event
+        }
     }
 
     render() {
+        const {title, id, where, url } = this.props.event;
         return (
             <div>
                 <Link href="/event-list">
                     <a>Event List</a>
                 </Link>
-                <h1>{this.props.id}</h1>
+                <div>
+                    <p>
+                        <span>
+                            {where}:
+                        </span>
+                        <a href={url}>{title}</a>
+                    </p>
+                </div>
             </div>
         )
     }
